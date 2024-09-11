7e34104814f2f8e01d8c000113bd1a5080709764
Urban Pettersson <urbanpe88@gmail.com>
2024-09-11 08:10:53 +0200

    feat: add ssm/systemsmanager to routes

--- lib/extension/index.ts
+++ lib/extension/index.ts
@@ -7,8 +7,10 @@ import { getParameter, getParameters } from './ssm.js';
 const app = new Koa()
 const router = new Router()
 
-router.get('/parameters/:parameterName', getParameter)
-router.get('/parameters', getParameters)
+router.get('/systemsmanager/parameters/:parameterName', getParameter)
+router.get('/ssm/parameters/:parameterName', getParameter)
+router.get('/systemsmanager/parameters', getParameters)
+router.get('/ssm/parameters', getParameters)
 app
   .use(router.routes())
   .use(router.allowedMethods())
