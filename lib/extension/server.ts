5783c0db93496cc78bcb38a95e947fe1248124f5
Urban Pettersson <urbanpe88@gmail.com>
2024-09-12 15:15:43 +0200

    feat: add routes for secretsmanager, change how server gets started

--- lib/extension/server.ts
+++ lib/extension/server.ts
@@ -4,17 +4,18 @@ import Router from '@koa/router'
 import { getParameter, getParameters } from './ssm.js';
 import { logger } from './logging.js';
 import { variables } from './env.js';
+import { getSecretValue } from './sm.js';
 
 const router = new Router()
 
-router.use(async(ctx, next) => {
+router.use(async (ctx, next) => {
   const tokenHeader = ctx.headers['x-secrets-extension-token']
 
   if (!tokenHeader) {
     logger.error('Token header missing in request')
     ctx.throw(401, JSON.stringify({ message: 'Missing auth header "X-Secrets-Extension-Token"' }))
   }
-  
+
   if (tokenHeader !== process.env.AWS_SESSION_TOKEN) {
     logger.error('Token header does not have same value as AWS_SESSION_TOKEN')
     ctx.throw(401, JSON.stringify({ message: 'Supplied auth header has incorrect value' }))
@@ -28,13 +29,14 @@ router.get('/ssm/parameters/:parameterName', getParameter)
 router.get('/systemsmanager/parameters', getParameters)
 router.get('/ssm/parameters', getParameters)
 
+router.get('/secretsmanager/secrets/:secretId', getSecretValue)
+router.get('/sm/secrets/:secretId', getSecretValue)
+
 const app = new Koa()
 
 app
   .use(router.routes())
   .use(router.allowedMethods())
 
-export const startServer = () => {
-  app.listen(variables.HTTP_PORT)
-  logger.info(`AWS Secrets Extension server is ready to receive requests on port ${variables.HTTP_PORT}`)
-}
+app.listen(variables.HTTP_PORT)
+logger.info(`AWS Secrets Extension server is ready to receive requests on port ${variables.HTTP_PORT}`)
