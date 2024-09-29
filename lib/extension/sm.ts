9d87a8bf7cc39e25da204db0e9f29f29b801ebe6
Urban Pettersson <urbanpe88@gmail.com>
2024-09-29 17:04:23 +0200

    feat: add support for setting region for sm secrets

--- lib/extension/sm.ts
+++ lib/extension/sm.ts
@@ -1,15 +1,18 @@
 import { GetSecretValueCommand, SecretsManagerClient } from "@aws-sdk/client-secrets-manager";
 import z from 'zod'
 
-import { Getter, GetterParams, KoaContext, KoaNext } from "./koa.js";
+import { Getter, GetterParams } from "./koa.js";
 import { logger } from "./logging.js";
 import { cache } from "./cache.js";
 import { variables } from "./env.js";
 import { stringIntegerSchema, validate } from "./validation.js";
+import { getRegion } from "./region.js";
 
-const client = new SecretsManagerClient({
+const clients: Record<string, SecretsManagerClient> = {}
+const getClient = (region: string) => clients[region] ??= new SecretsManagerClient({
+  region,
   requestHandler: {
-    requestTimeout: variables.SM_TIMEOUT,
+    requestTimeout: variables.SSM_TIMEOUT,
     httpsAgent: {
       maxSockets: variables.MAX_CONNECTIONS
     }
@@ -20,18 +23,20 @@ const getSecretValueSchema = z.object({
   secretId: z.string(),
   versionId: z.string().optional(),
   versionStage: z.string().optional(),
-  ttl: stringIntegerSchema.min(-1).default(variables.SM_TTL)
+  ttl: stringIntegerSchema.min(-1).default(variables.SM_TTL),
+  region: z.string().default(process.env.AWS_REGION!),
 })
 
 export const getSecretValue: Getter = async (params: GetterParams) => {
-  const { secretId: id, versionId, versionStage, ttl } = validate(getSecretValueSchema, params)
+  const { secretId: id, versionId, versionStage, ttl, region: regionParameter } = validate(getSecretValueSchema, params)
+  const region = getRegion(id, regionParameter)
   logger.debug(`Retrieving SM Secret ${id}`)
 
   return cache.getOrRetrieve({
     service: 'sm',
     key: id,
     ttl,
-    getter: () => client.send(new GetSecretValueCommand({
+    getter: () => getClient(region).send(new GetSecretValueCommand({
       SecretId: id,
       VersionId: versionId,
       VersionStage: versionStage
