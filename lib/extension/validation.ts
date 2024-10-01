4aa8e83bc146341726bf0a9d1b715bea3da4db91
Urban Pettersson <urbanpe88@gmail.com>
2024-10-01 08:19:21 +0200

    feat: add function for getting base parameters schema

--- lib/extension/validation.ts
+++ lib/extension/validation.ts
@@ -7,6 +7,12 @@ export const stringBooleanSchema = z.enum(["true", "false"])
 
 export const stringIntegerSchema = z.coerce.number().int()
 
+export const getBaseParameters = (defaultTtl: number) => z.object({
+  cacheKey: z.string().optional(),
+  region: z.string().optional(),
+  ttl: stringIntegerSchema.min(-1).default(defaultTtl)
+})
+
 export const validate = <T extends ZodSchema>(schema: T, input: unknown): z.output<T> => {
   const { error, data } = schema.safeParse(input)
   if (error) {
