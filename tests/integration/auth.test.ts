2cf3a0bfb72ece93b811933881ee8411a583a815
Urban Pettersson <urbanpe88@gmail.com>
2024-10-21 13:59:12 +0200

    test: improve handling of docker compose

--- tests/integration/auth.test.ts
+++ tests/integration/auth.test.ts
@@ -1,12 +1,18 @@
 import { exec, execSync } from 'child_process'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import waitOn from 'wait-on'
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 describe('Auth', () => {
   beforeAll(async () => {
-    exec('docker compose --file local/compose.yml up')
-    await sleep(10000)
+    exec('docker compose --file local/compose.yml up -d')
+    await waitOn({
+      resources: [
+        'tcp:3000'
+      ],
+    })
+    await sleep(1000)
   })
 
   afterAll(() => {
@@ -21,27 +27,27 @@ describe('Auth', () => {
       message: 'Missing auth header "X-Secrets-Extension-Token"',
     })
   })
-
+  
   it('should handle bad auth header', async () => {
     const headers = new Headers()
     headers.append('X-Secrets-Extension-Token', 'nope')
     const output = await fetch('http://localhost:2773/ssm/parameters/nope', {
       headers,
     })
-
+    
     expect(output.status).toEqual(401)
     expect(await output.json()).toEqual({
       message: 'Supplied auth header has incorrect value',
     })
   })
-
+  
   it('should handle non-existing resources', async () => {
     const headers = new Headers()
     headers.append('X-Secrets-Extension-Token', 'local-testing')
     const output = await fetch('http://localhost:2773/nope', {
       headers,
     })
-
+    
     expect(output.status).toEqual(404)
     expect(await output.json()).toEqual({
       message: 'No such resource, please review documentation for available resources',
